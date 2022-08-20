72d51743940b09fb76291fe6341836d320357e57
Jorge <jorge@jorge-VirtualBox>
2022-08-20 14:03:46 -0300

    onceagain

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,5 @@
-import link from 'next/link';
-
 function Home() {
-    return (<div>
-        <h1>Home</h1>
-
-        <link href="/paranat">
-            <a>Acessar area VIP</a>
-        </link>
-        </div>)
+    return <div>Home</div>
 }
 
 export default Home
